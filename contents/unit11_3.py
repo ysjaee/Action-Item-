f62ecabefac51d9410f0b76672e0fab0e90932ff
ysjaee <95131732+ysjaee@users.noreply.github.com>
2022-07-01 14:52:48 +0900

    Add files via upload

---
+++ contents/unit11_3.py
@@ -0,0 +1,15 @@
+a= [38,21, 53, 62, 19]
+print(a[0])
+print(a[2])
+print(a[4])
+print(a[-1])
+print(a[-5])
+# 리스트 부분 지우기
+del a[2]
+print(a)
+b =(38,21,53,62,19)
+print(b[0])
+r=range(0,10,2)
+print(r[2])
+hello = 'hello, world!'
+print(hello[7])
---
+++ contents/unit14_2.py
@@ -0,0 +1,25 @@
+# if 조건문에 문자열 지정하기
+if 'Hello':
+    print('참')
+else:
+    print('거짓')
+
+if '':
+    print('참')
+else:
+    print('거짓')
+
+# not으로 결과 뒤집기
+if not 0:
+    print('참')
+else:
+    print('거짓')
+if not None:
+    print('참')
+else:
+    print('거짓')
+
+if not '':
+    print('참')
+else:
+    print('거짓')
